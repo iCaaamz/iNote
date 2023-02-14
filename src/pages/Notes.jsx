10c921cb81ed2a3ce11703c2a08078894a78e6fb
¨¨iCaaamz¨ <camalvaro15@gmail.com>
2023-02-14 18:51:49 -0500

    Create the repo

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {CiSearch} from "react-icons/ci"
+import dummyNotes from '../dummy__notes'
 const Notes = () => {
   return (
     <section>
         <header className="notes__header">
             <h2>iNotes</h2>
             <input type="text" autoFocus placeholder='Keyword...' />
-            <button><CiSearch/></button>
+            <button className='btn'><CiSearch/></button>
         </header>
+        <div className="notes__container">
+
+        </div>
     </section>
   )
 }
